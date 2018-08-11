6c3e27724271d4b305deb89b2659b54c85278bc8
vdayanand <venkatesh@juliacomputing.com>
2018-08-11 18:11:28 +0100

    fix test

--- test/client_test.jl
+++ test/client_test.jl
@@ -46,7 +46,7 @@ catch err
     global caughterr = err
 end
 @test typeof(caughterr) <: WebSockets.WebSocketClosedError
-@test caughterr.message == " while open ws|client: Base.IOError(\"connect: connection refused (ECONNREFUSED)\", -111)"
+@test  occursin("while open ws|client: Base.IOError(\"connect: connection refused (ECONNREFUSED)\"", caughterr.message)
 
 @info("try open with uknown scheme\n")
 sleep(1)
